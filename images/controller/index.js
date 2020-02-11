b3fcef2793db9d56b9c0b6fe0d6eb583f2d086d1
Christian Werner <ccwerner96@gmail.com>
2020-02-11 15:02:41 -0300

    changed text from seed

--- images/controller/index.js
+++ images/controller/index.js
@@ -7,27 +7,27 @@ const images = [
 	{
 		src: "https://www.atomix.com.au/media/2017/07/StockPhotoBanner.jpg",
 		alt: "image",
-		text: "lorem ipsum"
+		text: "dolor sit amet"
 	},
 	{
 		src: "https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F550533%2Frocket-man.jpg&w=700&op=resize",
 		alt: "image",
-		text: "lorem ipsum"
+		text: "consectetur adipiscing elit"
 	},
 	{
 		src: "https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F479691%2Fconfused.jpg&w=700&op=resize",
 		alt: "image",
-		text: "lorem ipsum"
+		text: "sed do eiusmod"
 	},
 	{
 		src: "https://cdn.lolwot.com/wp-content/uploads/2015/06/18-hilarious-and-bizarre-stock-photos-1.jpg",
 		alt: "image",
-		text: "lorem ipsum"
+		text: "tempor incididunt"
 	},
 	{
 		src: "https://www.demilked.com/magazine/wp-content/uploads/2018/03/5aaa1cba74cb3-funny-weird-wtf-stock-photos-24-5a392d5d9bda7__700.jpg",
 		alt: "image",
-		text: "lorem ipsum"
+		text: "ut labore et dolore"
 	}
 ];
 
